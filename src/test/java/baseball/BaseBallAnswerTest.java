02165aeb0c34ffa552d48ed469eb680a8e231419
boss.back <boss.back@kakaocorp.com>
2022-04-18 01:12:57 +0900

    feat: Set 을 사용하여 야구게임의 정답이 서로 다른 숫자로 구성되었는지 확인하는 테스트 추가

---
+++ src/test/java/baseball/BaseBallAnswerTest.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseBallAnswerTest {
+
+    @DisplayName("야구 게임의 정답이 서로 다른 숫자가 생성되었는지 확인한다.")
+    @Test
+    public void create_different_number_test() {
+        BaseBallAnswer baseBallAnswer = new BaseBallAnswer();
+        List<Integer> answer = baseBallAnswer.getAnswer();
+        Set<Integer> answerSet = new HashSet<>(answer);
+        assertThat(answer.size()).isEqualTo(answerSet.size());
+    }
+}
